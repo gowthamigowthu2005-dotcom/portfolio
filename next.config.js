505d6b6371fd2e4fd8dd7eb7afd78a5a5ad3bb34
gowthamigowthu2005-dotcom <gowthamigowthu2005@gmail.com>
2026-07-28 20:35:17 +0530

    Fix repoName casing in next.config.js to portfolio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
-const repoName = "Portfolio";
+const repoName = "portfolio";
 
 const nextConfig = {
   output: "export",
